2613c97bd996863425b6927d028922de7957816a
Jonathan Witman <jawitman8@gmail.com>
2019-01-31 12:47:26 -0500

    linked module file, created base skeleton for game

---
+++ pythongame.py
@@ -0,0 +1,32 @@
+import module
+module.apple()
+
+class Map(object):
+
+    rooms = {
+
+    }
+
+class Runner(object):
+
+    pass
+
+
+class Death(object):
+
+    pass
+
+
+class room1(object):
+
+    pass
+
+
+class room2(object):
+
+    pass
+
+
+class room3(object):
+
+    pass
