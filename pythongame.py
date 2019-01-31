130c092df20c69a5af9d29458667818128ca3c8f
Jonathan Witman <jawitman8@gmail.com>
2019-01-31 15:16:22 -0500

    began filling in skeleton

--- pythongame.py
+++ pythongame.py
@@ -1,32 +1,49 @@
 import module
-module.apple()
 
-class Map(object):
 
-    rooms = {
+def next_room(self, room):
 
-    }
+    self.room = room
+
 
 class Runner(object):
 
-    pass
+    def __init__(self, room):
+        self.room = room
 
 
 class Death(object):
 
+    def dialouge():
+        print("You died my child")
+        exit(1)
+
+
+class FrontPorch(object):
+
+    def dialouge():
+        print("front porch dialouge 1 die 2 liv")
+        answer = input("> ")
+
+        if answer == '1':
+            print("u die")
+            Death.dialouge()
+#return some value
+
+class Lobby(object):
+
     pass
 
 
-class room1(object):
+class Basement(object):
 
     pass
 
+class Map(object):
 
-class room2(object):
+    rooms = {
+        'death' : Death(),
+        'front_porch' : FrontPorch(),
+    }
 
-    pass
-
-
-class room3(object):
-
-    pass
+FrontPorch.dialouge()
